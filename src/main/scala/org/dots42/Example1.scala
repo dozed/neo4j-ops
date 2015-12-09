957b8a56b649bb96cd372f7adf84f4130535154d
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-12-09 21:32:10 +0100

    Created an example.

---
+++ src/main/scala/org/dots42/Example1.scala
@@ -0,0 +1,55 @@
+package org.dots42
+
+import org.dots42.neo4j.Neo4j
+import org.neo4j.graphdb.{Result, GraphDatabaseService}
+
+import scalaz._, Scalaz._
+import scalaz.concurrent.Task
+
+object Example1 extends App {
+
+  import neo4j.Connections._
+  import neo4j.Queries._
+  import neo4j.Parsers._
+
+  case class Foo(bar: String, baz: String)
+
+  def createFoo(bar: String, baz: String): ConnectionIO[Result] = {
+    query(
+      """
+        |create (f:Foo {
+        |  bar: {bar},
+        |  baz: {baz}
+        |})
+      """.stripMargin, Map("bar" -> bar, "baz" -> baz))(resultItemParser).result
+  }
+
+  val fooParser: Parser[Foo] = (parse[String]("bar") |@| parse[String]("baz")).tupled.map {
+    case (bar, baz) => Foo(bar, baz)
+  }
+
+  val listFoos: ConnectionIO[List[Foo]] = query(
+    """
+      |match (f:Foo)
+      |return
+      |  f.bar as bar,
+      |  f.baz as baz
+    """.stripMargin)(fooParser).list
+
+
+  val x: ConnectionIO[List[Foo]] = for {
+    _ <- createFoo("hey", "ho")
+    xs <- listFoos
+  } yield xs
+
+  val db = Neo4j.graphDatabaseService
+  def con(db: GraphDatabaseService): Connection = Connection(db)
+
+  val t: Task[List[Foo]] = x.transact(con(db))
+
+  println(t.run)
+  // List(Foo(hey,ho))
+
+
+}
+
--- src/main/scala/org/dots42/Example2.scala
+++ src/main/scala/org/dots42/Example2.scala
@@ -4,79 +4,14 @@ import org.dots42.Data._
 import org.dots42.WorldTask._
 import org.dots42.neo4j.Connections.{ConnectionIO, Connection}
 import org.dots42.neo4j.Neo4j
-import org.neo4j.graphdb.{Result, GraphDatabaseService}
+import org.neo4j.graphdb.GraphDatabaseService
 
 import scalaz._, Scalaz._
 import scalaz.concurrent.Task
 
 
-
-object Example1 extends App {
-
-  import neo4j.Connections._
-  import neo4j.Queries._
-  import neo4j.Parsers._
-
-  case class Foo(bar: String, baz: String)
-
-  def createFoo(bar: String, baz: String): ConnectionIO[Result] = {
-    query(
-      """
-        |create (f:Foo {
-        |  bar: {bar},
-        |  baz: {baz}
-        |})
-      """.stripMargin, Map("bar" -> bar, "baz" -> baz))(resultItemParser).result
-  }
-
-  val fooParser: Parser[Foo] = (parse[String]("bar") |@| parse[String]("baz")).tupled.map {
-    case (bar, baz) => Foo(bar, baz)
-  }
-
-  val listFoos: ConnectionIO[List[Foo]] = query(
-    """
-      |match (f:Foo)
-      |return
-      |  f.bar as bar,
-      |  f.baz as baz
-    """.stripMargin)(fooParser).list
-
-
-  val x: ConnectionIO[List[Foo]] = for {
-    _ <- createFoo("hey", "ho")
-    xs <- listFoos
-  } yield xs
-
-  val db = Neo4j.graphDatabaseService
-  def con(db: GraphDatabaseService): Connection = Connection(db)
-
-  val t: Task[List[Foo]] = x.transact(con(db))
-
-  println(t.run)
-  // List(Foo(hey,ho))
-
-
-}
-
-
-
-case class World(db: GraphDatabaseService) {
-
-  def con = Connection(db)
-
-}
-
-object WorldTask {
-
-  // computations are World => Task[A] (fused by Kleisli)
-  type WorldTask[A] = Kleisli[Task[?], World, A]
-
-  def task[A](f: World => Task[A]): Kleisli[Task[?], World, A] = Kleisli[Task[?], World, A](f)
-
-}
-
-
-object MyApp extends App {
+// computations are World => Task[A] (fused by Kleisli)
+object Example2 extends App {
 
   // runs in a single transaction
   val p1 = DocumentModule.listCreateListDocuments("foo", Privacy.Public)
@@ -96,6 +31,21 @@ object MyApp extends App {
   println(res2)
 
 }
+
+case class World(db: GraphDatabaseService) {
+
+  def con = Connection(db)
+
+}
+
+object WorldTask {
+
+  type WorldTask[A] = Kleisli[Task[?], World, A]
+
+  def task[A](f: World => Task[A]): Kleisli[Task[?], World, A] = Kleisli[Task[?], World, A](f)
+
+}
+
 
 // each function defines a transaction boundary
 object DocumentModule {
