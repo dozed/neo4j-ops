f4b21c7b75efb687bf9dd08ff08ea368002aa80a
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2016-01-23 20:59:27 +0100

    Make Parsers monadic.

--- src/main/scala/org/dots42/neo4j/Parsers.scala
+++ src/main/scala/org/dots42/neo4j/Parsers.scala
@@ -1,54 +1,64 @@
 package org.dots42.neo4j
 
-import org.dots42.Data.Privacy
+import org.neo4j.cypher.QueryStatistics
 
+import scala.util.Try
 import scalaz._, Scalaz._
 
+// Parser[A] converts from Map[String, Any] to A
+// - decoders
+// - 1-step navigation
 object Parsers {
 
   import Connections._
+  import Decoders._
 
-  // optics?
   trait ParseGen[I, A]
 
   case class Parser[A](run: ResultItem => A) extends ParseGen[ResultItem, A]
 
-  case class Decoder[A](run: AnyRef => A) extends ParseGen[ResultItem, A]
 
-  // traversal + parser
+
+  implicit val parserMonad = new Monad[Parser] {
+    override def point[A](a: => A): Parser[A] = Parser[A](_ => a)
+
+    override def bind[A, B](fa: Parser[A])(f: (A) => Parser[B]): Parser[B] = Parser[B] { res =>
+      val a = fa.run(res)
+      val b = f(a).run(res)
+      b
+    }
+  }
+
+
+  // default parser
+  implicit val resultItemParser = Parser[ResultItem](identity)
+
+
+  // primitive parser (+1-step traversal)
   def parse[A: Decoder](key: String): Parser[A] = Parser[A] { m =>
     implicitly[Decoder[A]].run(m(key))
   }
 
-
-  implicit val parserApplicative = new Applicative[Parser] {
-    override def point[A](a: => A): Parser[A] = Parser[A](_ => a)
-
-    // apply a function, e.g. collect of multiple Applicatives to tuples
-    override def ap[A, B](fa: => Parser[A])(f: => Parser[(A) => B]): Parser[B] = Parser[B](m => f.run(m)(fa.run(m)))
+  // -> ParseResult and |
+  def parseOrElse[A: Decoder](key: String, default: A): Parser[A] = Parser[A] { m =>
+    Try(implicitly[Decoder[A]].run(m(key))).getOrElse(default)
   }
 
-  implicit val decoderFunctor = new Functor[Decoder] {
-    override def map[A, B](fa: Decoder[A])(f: (A) => B): Decoder[B] = Decoder[B](x => f(fa.run(x)))
+
+  // parser for common arities
+  def parse1[A1:Decoder](k1: String) = parse[A1](k1)
+
+  def parse2[A1:Decoder, A2:Decoder](k1: String, k2: String) = {
+    (parse[A1](k1) |@| parse[A2](k2)).tupled
   }
 
-  def decoderByCast[A]: Decoder[A] = Decoder[A](_.asInstanceOf[A])
+  def parse3[A1:Decoder, A2:Decoder, A3:Decoder](k1: String, k2: String, k3: String) = {
+    (parse[A1](k1) |@| parse[A2](k2) |@| parse[A3](k3)).tupled
+  }
 
-  def decoder[A](f: AnyRef => A): Decoder[A] = Decoder[A](f)
-
-
-
-  // some concrete parsers/decoders
-
-  implicit def stringDecoder: Decoder[String] = decoderByCast[String]
-  implicit def intDecoder: Decoder[Int] = decoderByCast[Int]
-
-  implicit val structureTypeDecoder: Decoder[Privacy] = stringDecoder map Privacy.fromString
-
-  implicit val resultItemParser = Parser[ResultItem](identity)
-
-  // parse first value of set
-  implicit val stringParser = Parser[String](m => m.values.head.asInstanceOf[String])
+  def parse4[A1:Decoder, A2:Decoder, A3:Decoder, A4:Decoder](k1: String, k2: String, k3: String, k4: String) = {
+    (parse[A1](k1) |@| parse[A2](k2) |@| parse[A3](k3) |@| parse[A4](k4)).tupled
+  }
 
 
 }
